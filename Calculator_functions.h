c06a811cb3beee305e02aea1f7850cc702d31598
Alex Ferdinand <98086926+Ferdinand3043@users.noreply.github.com>
2025-12-25 14:23:40 -0500

    Add files via upload

---
+++ Calculator_functions.h
@@ -0,0 +1,12 @@
+int add(int num1, int num2);
+
+int sub(int num1, int num2);
+
+int divi(int num1, int num2);
+
+int mul(int num1, int num2);
+
+int add_frac(int nom1, int dom2, int nom2, int dom1);
+
+int sub_frac(int nom1, int dom2, int nom2, int dom1);
+
