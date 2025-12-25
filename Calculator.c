4cf03cbbdda0d5c4d11eae5a9272ebbd727da6e0
Alex Ferdinand <98086926+Ferdinand3043@users.noreply.github.com>
2025-12-25 14:25:36 -0500

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,41 @@
+int num1, num2, addition, subtraction, division, multiplication;
+int nom1, dom1, nom2, dom2, numerator, denominator,fraction_1, fraction_2;
+char answer[10], answer2[10], answer3[10], answer4[10],fnum, snum;
+
+int add(int num1, int num2){
+
+    return num1 + num2;
+}
+
+int sub(int num1, int num2){
+
+    return num1 - num2;
+}
+
+int divi(int num1, int num2){
+
+    return num1 / num2;
+}
+
+int mul(int num1, int num2){
+
+    return num1 * num2;
+
+}
+
+int add_frac(int nom1, int dom2, int nom2, int dom1){
+
+    return numerator = (nom1 * dom2) + (nom2 * dom1);
+           denominator = dom1 * dom2;
+           fraction_1 = numerator;
+           fraction_2 = denominator;
+}
+
+int sub_frac(int nom1, int dom2, int nom2, int dom1){
+
+    return numerator = (nom1 * dom2) - (nom2 * dom1);
+    return denominator = dom1 * dom2;
+    return fraction_1 = numerator;
+    return fraction_2 = denominator;
+
+}
